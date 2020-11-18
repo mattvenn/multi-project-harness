54fd7c77e92943e96b4e1c50ef2400e04a35babd
Richard Miller <miller.research@gmail.com>
2020-11-18 16:01:08 +0000

    Add spinet as project 3

--- top.v
+++ top.v
@@ -3,7 +3,7 @@
 module multi_project_harness #(
     parameter address_active = 32'h00FF00FF,  // TODO
     parameter address_ws2812 = 32'h00FF00FA,  // TODO
-    parameter num_projects   = 3
+    parameter num_projects   = 4
 ) (
     inout vdda1,	// User area 1 3.3V supply
     inout vdda2,	// User area 2 3.3V supply
@@ -53,18 +53,21 @@ module multi_project_harness #(
     assign io_out = active_project == 0 ? project_io_out[0] :
                     active_project == 1 ? project_io_out[1] :
                     active_project == 2 ? project_io_out[2] :
+                    active_project == 3 ? project_io_out[3] :
                                           `MPRJ_IO_PADS'b0;
 
     // each project sets own oeb
     assign io_oeb = active_project == 0 ? project_io_out[0] :
                     active_project == 1 ? project_io_out[1] :
                     active_project == 2 ? project_io_out[2] :
+                    active_project == 3 ? project_io_out[3] :
                                           `MPRJ_IO_PADS'b0;
 
     // inputs get set to z if not selected
     assign project_io_in[0] = active_project == 0 ? io_in : `MPRJ_IO_PADS'bz;
     assign project_io_in[1] = active_project == 1 ? io_in : `MPRJ_IO_PADS'bz;
     assign project_io_in[2] = active_project == 2 ? io_in : `MPRJ_IO_PADS'bz;
+    assign project_io_in[3] = active_project == 3 ? io_in : `MPRJ_IO_PADS'bz;
 
 
     // instantiate all the modules
@@ -73,6 +76,18 @@ module multi_project_harness #(
     // ws2812 needs led_num, rgb, write connected to wb
     ws2812                proj_1 (.clk(project_io_in[1][0]), .reset(project_io_in[1][1]), .led_num(wbs_dat_i[31:24]), .rgb_data(wbs_dat_i[23:0]), .write(ws2812_write), .data(project_io_out[1][32]));
     vga_clock             proj_2 (.clk(project_io_in[2][0]), .reset_n(!project_io_in[2][1]), .adj_hrs(project_io_in[2][2]), .adj_min(project_io_in[2][3]), .adj_sec(project_io_in[2][4]), .hsync(project_io_out[2][5]), .vsync(project_io_out[2][6]), .rrggbb(project_io_out[2][12:7]));
+	wire [13:0] p3in, p3out;
+	assign p3in = project_io_in[3][13:0];
+	assign project_io_out[3][13:0] = p3out;
+	spinet #(.N(2), .WIDTH(16), .ABITS(3)) proj_3 (
+		.clk(p3in[0]),
+		.rst(p3in[1]),
+		.MOSI(p3in[3:2]),
+		.SCK(p3in[5:4]),
+		.SS(p3in[7:6]),
+		.MISO(p3out[9:8]),
+		.txready(p3out[11:10]),
+		.rxready(p3out[13:12]));
     
     reg [7:0] active_project = 0; // which design is active
 
