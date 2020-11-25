75c6e9754a4ce76a672ae527ecfeb2735c083b8a
Matt Venn <matt@mattvenn.net>
2020-11-25 12:57:00 +0100

    macro placement and config update

--- openlane/config/config.tcl
+++ openlane/config/config.tcl
@@ -1,13 +1,17 @@
 # User config
 set ::env(DESIGN_NAME) multi_project_harness
+set script_dir [file dirname [file normalize [info script]]]
 
 set ::env(FP_CORE_UTIL) 15
 
 set ::env(FP_PDN_VOFFSET) 0
 set ::env(FP_PDN_VPITCH) 30
 
-# doesn't work
-#set ::env(VERILOG_FILES_BLACKBOX) "seven_segment_seconds ws2812 vga_clockv"
+# make the project harness include blackbox.v
+set ::env(SYNTH_DEFINES) "BLACKBOX"
+
+set ::env(VERILOG_FILES) "\
+    $script_dir/src/multi_project_harness.v"
 
 set ::env(MACRO_PLACEMENT_CFG) $::env(OPENLANE_ROOT)/designs/$::env(DESIGN_NAME)/macro_placement.cfg
 
@@ -22,8 +26,6 @@ set ::env(CLOCK_TREE_SYNTH) 0
 
 set ::env(DIODE_INSERTION_STRATEGY) 0
 
-# Change if needed
-set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/*.v]
 set ::env(EXTRA_LEFS) [glob $::env(DESIGN_DIR)/macros/lef/*.lef]
 set ::env(EXTRA_GDS_FILES) [glob $::env(DESIGN_DIR)/macros/gds/*.gds]
 
