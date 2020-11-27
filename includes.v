abc1d076bbed68a1e83a955712928bf3a8f45c44
Bynaryman <i.f.lledoux@gmail.com>
2020-11-27 13:15:44 +0100

    moved includes.v and multi_project_harness.v to the correct place

--- includes.v
+++ includes.v
@@ -8,4 +8,11 @@
 `include "multi_project_harness/vga-clock/rtl/vga_clock.v"
 `include "multi_project_harness/spinet/rtl/spinet.v"
 `include "multi_project_harness/asicfreq/asic_freq.v"
+`include "multi_project_harness/ASIC_watch/submodules/count10m.v"
+`include "multi_project_harness/ASIC_watch/submodules/count24h.v"
+`include "multi_project_harness/ASIC_watch/submodules/count60m.v"
+`include "multi_project_harness/ASIC_watch/submodules/count60s.v"
+`include "multi_project_harness/ASIC_watch/submodules/crystal2hz.v"
+`include "multi_project_harness/ASIC_watch/submodules/segment7.v"
+`include "multi_project_harness/ASIC_watch/hdl/watch_hhmm.v"
 
