a8fc0c4ebeaeffab63aa289eaadf83261d75eadb
Matt Venn <matt@mattvenn.net>
2020-11-30 11:55:46 +0100

    reports

--- reports.py
+++ reports.py
@@ -10,18 +10,20 @@ openlane_designs = os.path.join(os.environ['OPENLANE_ROOT'], 'designs')
 gds_dir = "../../gds/mph/"
 lef_dir = "../../lef/mph/"
 
-designs = ["ws2812", "vga_clock", "seven_segment_seconds", "spinet5", "asic_freq", "watch_hhmm"]
+designs = ["ws2812", "vga_clock", "seven_segment_seconds", "spinet5", "asic_freq", "watch_hhmm", "challenge"]
 
 def report():
     for design in designs:
         run_dir = os.path.join(openlane_designs, design, 'runs/*')
         list_of_files = glob.glob(run_dir)
         latest_run = max(list_of_files, key=os.path.getctime)
-        print("## %s" % design)
+        date = os.path.basename(latest_run)
+        print("## %s : DESIGN=%s RUN_DATE=%s" % (design, design, date))
         print()
 
         summary_file = os.path.join(latest_run, 'reports', 'final_summary_report.csv')
 
+        # print pertinent summary - only interested in errors atm
         with open(summary_file) as fh:
             summary = csv.DictReader(fh)
             for row in summary:
@@ -29,7 +31,16 @@ def report():
                     if "violation" in key or "error" in key:
                         print("%30s : %20s" % (key, value))
 
-       
+      
+        # what drc is broken?
+        print()
+        drc_file = os.path.join(latest_run, 'logs', 'magic', 'magic.drc')
+        with open(drc_file) as drc:
+            for line in drc.readlines():
+                if '(' in line:
+                    print("* " + line)
+
+        # copy files
         if args.copy_files:
             gds_file = os.path.join(latest_run, 'results', 'magic', design + ".gds")
             lef_file = os.path.join(latest_run, 'results', 'magic', design + ".lef")
