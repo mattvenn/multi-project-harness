da918cf341c36ef40a9962acf1f33b8bf30587fa
matt venn <matt@mattvenn.net>
2020-12-04 11:41:56 +0100

    wip openlane config

--- openlane/config/config.tcl
+++ openlane/config/config.tcl
@@ -1,35 +1,42 @@
 # User config
 set ::env(DESIGN_NAME) multi_project_harness
-set script_dir [file dirname [file normalize [info script]]]
-
-set ::env(FP_CORE_UTIL) 15
-
-set ::env(FP_PDN_VOFFSET) 0
-set ::env(FP_PDN_VPITCH) 30
 
 # make the project harness include blackbox.v
-set ::env(SYNTH_DEFINES) "BLACKBOX"
+set ::env(SYNTH_DEFINES) "BLACKBOX NO_PROJ1 NO_PROJ2 NO_PROJ3 NO_PROJ4 NO_PROJ5 NO_PROJ6 NO_PROJ7"
 
-set ::env(VERILOG_FILES) "\
-    $script_dir/src/multi_project_harness.v"
+# Change if needed
+set ::env(VERILOG_FILES) $::env(DESIGN_DIR)/mpw-multi-project-harness/multi_project_harness.v
 
-set ::env(MACRO_PLACEMENT_CFG) $::env(OPENLANE_ROOT)/designs/$::env(DESIGN_NAME)/macro_placement.cfg
+set ::env(DESIGN_IS_CORE) 0
+set ::env(FP_PDN_CORE_RING) 0
+set ::env(GLB_RT_MAXLAYER) 5
 
-
-set ::env(PL_TARGET_DENSITY) 0.35
-set ::env(PL_BASIC_PLACEMENT) 1
-set ::env(CELL_PAD) 0
-
-set ::env(CLOCK_PERIOD) "20"
+# Fill this
+# 50Mhz
+set ::env(CLOCK_PERIOD) "20" 
 set ::env(CLOCK_PORT) "wb_clk_i"
-set ::env(CLOCK_TREE_SYNTH) 0
 
-set ::env(DIODE_INSERTION_STRATEGY) 0
+#set ::env(PL_RANDOM_GLB_PLACEMENT) 0
+set ::env(PL_BASIC_PLACEMENT) 0
+set ::env(PL_SKIP_INITIAL_PLACEMENT) 1
 
+# macro stuff
+set ::env(MACRO_PLACEMENT_CFG) $::env(OPENLANE_ROOT)/designs/$::env(DESIGN_NAME)/macro_placement.cfg
 set ::env(EXTRA_LEFS) [glob $::env(DESIGN_DIR)/macros/lef/*.lef]
 set ::env(EXTRA_GDS_FILES) [glob $::env(DESIGN_DIR)/macros/gds/*.gds]
+set ::env(FP_HORIZONTAL_HALO) 25
+set ::env(FP_VERTICAL_HALO) 20
+
+set ::env(FP_SIZING) absolute
+set ::env(DIE_AREA) "0 0 400 600"
+set ::env(PL_TARGET_DENSITY) 0.3
+
+# CORE_UTIL not used if FP_SIZING is absolute
+#set ::env(FP_CORE_UTIL) 35
+#set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
 
 set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
 if { [file exists $filename] == 1} {
 	source $filename
 }
+
