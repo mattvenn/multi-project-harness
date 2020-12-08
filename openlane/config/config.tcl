586e6a3782aa4b6af4484b3536d9fd1cb85a982c
matt venn <matt@mattvenn.net>
2020-12-08 18:29:35 +0100

    latest config

--- openlane/config/config.tcl
+++ openlane/config/config.tcl
@@ -5,19 +5,17 @@ set ::env(DESIGN_NAME) multi_project_harness
 set ::env(SYNTH_DEFINES) "BLACKBOX" 
 # NO_PROJ0 NO_PROJ1 NO_PROJ2 NO_PROJ3 NO_PROJ4 NO_PROJ5 NO_PROJ6"
 
+
 # Change if needed
 set ::env(VERILOG_FILES) $::env(DESIGN_DIR)/mpw-multi-project-harness/multi_project_harness.v
 
 set ::env(DESIGN_IS_CORE) 1
-#set ::env(FP_PDN_CORE_RING) 0
-#set ::env(GLB_RT_MAXLAYER) 5
 
 # Fill this
 # 50Mhz
 set ::env(CLOCK_PERIOD) "20" 
 set ::env(CLOCK_PORT) "wb_clk_i"
 
-#set ::env(PL_RANDOM_GLB_PLACEMENT) 0
 set ::env(PL_BASIC_PLACEMENT) 0
 set ::env(PL_SKIP_INITIAL_PLACEMENT) 1
 set ::env(DIODE_INSERTION_STRATEGY) 0
@@ -29,13 +27,13 @@ set ::env(EXTRA_GDS_FILES) [glob $::env(DESIGN_DIR)/macros/gds/*.gds]
 set ::env(FP_HORIZONTAL_HALO) 10
 set ::env(FP_VERTICAL_HALO) 10
 
+# was having lvs issues with cells in the extreme left and right not getting a vertical power line because the pitch was too wide
+set ::env(FP_PDN_VPITCH) 75
+
+# CORE_UTIL not used if FP_SIZING is absolute
 set ::env(FP_SIZING) absolute
 set ::env(DIE_AREA) "0 0 1200 1500"
 set ::env(PL_TARGET_DENSITY) 0.8
-
-# CORE_UTIL not used if FP_SIZING is absolute
-#set ::env(FP_CORE_UTIL) 35
-#set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
 
 set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
 if { [file exists $filename] == 1} {
