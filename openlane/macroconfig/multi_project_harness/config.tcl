ebe188146c5399cbb60180e45776af119badd750
matt venn <matt@mattvenn.net>
2020-12-15 12:18:56 +0100

    update  config

--- openlane/macroconfig/multi_project_harness/config.tcl
+++ openlane/macroconfig/multi_project_harness/config.tcl
@@ -9,20 +9,20 @@ set ::env(DESIGN_IS_CORE) 0
 set ::env(FP_PDN_CORE_RING) 0
 set ::env(GLB_RT_MAXLAYER) 5
 
-set ::env(PL_RANDOM_GLB_PLACEMENT) 1
-
 # Fill this
-# 20Mhz
-set ::env(CLOCK_PERIOD) "50" 
+# 50Mhz
+set ::env(CLOCK_PERIOD) "20" 
 set ::env(CLOCK_PORT) "wb_clk_i"
 
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 2200 400"
-set ::env(PL_TARGET_DENSITY) 0.2
+set ::env(DIE_AREA) "0 0 1500 400"
+set ::env(PL_TARGET_DENSITY) 0.3
 
+set ::env(PL_SKIP_INITIAL_PLACEMENT) 1
+
+set ::env(FP_PIN_ORDER_CFG) $::env(DESIGN_DIR)/pin_order.cfg
 
 set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
 if { [file exists $filename] == 1} {
 	source $filename
 }
-
