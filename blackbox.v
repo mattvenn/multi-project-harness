9658664d441f41c4c20a9304eb73c0dc3b3a2fb7
matt venn <matt@mattvenn.net>
2020-12-17 12:48:40 +0100

    add power pins to blackbox

--- blackbox.v
+++ blackbox.v
@@ -105,6 +105,16 @@ module multi_project_harness #(
     parameter address_watch   = 32'h30000500,
     parameter num_projects   = 8 )
     (
+`ifdef USE_POWER_PINS
+    inout vdda1,	// User area 1 3.3V supply
+    inout vdda2,	// User area 2 3.3V supply
+    inout vssa1,	// User area 1 analog ground
+    inout vssa2,	// User area 2 analog ground
+    inout vccd1,	// User area 1 1.8V supply
+    inout vccd2,	// User area 2 1.8v supply
+    inout vssd1,	// User area 1 digital ground
+    inout vssd2,	// User area 2 digital ground
+`endif
     // Wishbone Slave ports (WB MI A)
     input wire wb_clk_i,             // clock
     input wire wb_rst_i,             // reset
